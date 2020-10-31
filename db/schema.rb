e79cf15f916c0a3bbf235a933d6b51e2836be5dc
Saddam <saddam.asmatullayev@nu.edu.kz>
2020-10-31 11:42:20 +0600

    last working version

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2020_10_17_094806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "s"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
