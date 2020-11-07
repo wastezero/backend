235f1855146dd22a0c23c81b18d1921194eb43be
daneker <d.nurgaliyeva@alabs.team>
2020-11-07 19:26:43 +0600

    added post method for order

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,11 @@ Rails.application.routes.draw do
         resources :branches
         resources :restaurants
       end
+      # resources :clients
       namespace :client do
         resources :orders
         resources :branches
+        resources :profile
       end
     end
   end
