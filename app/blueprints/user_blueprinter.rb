e9ee6f62100731b3abf3c59bf0757bf0c285b675
Saddam <saddam.asmatullayev@nu.edu.kz>
2020-11-22 12:55:42 +0600

    Fix who_am_i

--- app/blueprints/user_blueprinter.rb
+++ app/blueprints/user_blueprinter.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class UserBlueprinter < Blueprinter::Base
-  identifier :id
+  field :id do |user|
+    if user.restaurant_id.present?
+      user.restaurant_id
+    elsif user.manager_id.present?
+      user.manager_id
+    elsif user.admin_id.present?
+      user.admin_id
+    end
+  end
 
   fields :email, :authentication_token
 
